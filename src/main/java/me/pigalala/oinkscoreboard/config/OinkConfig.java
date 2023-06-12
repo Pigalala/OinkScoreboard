c005ad7092015e2350105dd845aeec621cd93ef7
Pigalala <w8888mc@gmail.com>
2023-06-12 16:07:02 +0100

    Update 1.20

--- src/main/java/me/pigalala/oinkscoreboard/config/OinkConfig.java
+++ src/main/java/me/pigalala/oinkscoreboard/config/OinkConfig.java
@@ -11,7 +11,6 @@ import java.io.FileWriter;
 public class OinkConfig {
 
     public static int scoreboardColour = 0x4c000000;
-    public static boolean showMaxRows = false;
     public static ScoreboardPlacements scoreboardPlacement = ScoreboardPlacements.NORMAL;
 
     private static File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "oinkscoreboard.properties");
@@ -24,8 +23,6 @@ public class OinkConfig {
                 do {
                     if(line.startsWith("scolour "))
                         scoreboardColour = Integer.parseInt(line.substring(8));
-                    else if(line.startsWith("showmaxrows "))
-                        showMaxRows = Boolean.parseBoolean(line.substring(12));
                     else if(line.startsWith("placement "))
                         scoreboardPlacement = ScoreboardPlacements.valueOf(line.substring(10));
                     line = br.readLine();
@@ -41,7 +38,6 @@ public class OinkConfig {
         try {
             FileWriter writer = new FileWriter(configFile);
             writer.write("scolour " + scoreboardColour + "\n");
-            writer.write("showmaxrows " + showMaxRows + "\n");
             writer.write("placement " + scoreboardPlacement.toString() + "\n");
             writer.close();
         }
--- src/main/java/me/pigalala/oinkscoreboard/mixins/InGameHudMixin.java
+++ src/main/java/me/pigalala/oinkscoreboard/mixins/InGameHudMixin.java
@@ -1,22 +1,18 @@
 package me.pigalala.oinkscoreboard.mixins;
 
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.mojang.datafixers.util.Pair;
-import me.pigalala.oinkscoreboard.OinkScoreboard;
 import me.pigalala.oinkscoreboard.ScoreboardPlacements;
 import me.pigalala.oinkscoreboard.config.OinkConfig;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
 import net.minecraft.scoreboard.Team;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,22 +24,18 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static net.minecraft.client.gui.DrawableHelper.fill;
-
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     @Shadow private int scaledWidth;
     @Shadow private int scaledHeight;
     @Shadow public abstract TextRenderer getTextRenderer();
 
-    @Shadow @Final private MinecraftClient client;
-
     @Inject(
             method = "renderScoreboardSidebar",
             at = @At(value = "HEAD"),
             cancellable = true
     )
-    private void renderScoreboardSidebar(MatrixStack matrices, ScoreboardObjective objective, CallbackInfo ci) {
+    private void renderScoreboardSidebar(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
         Scoreboard scoreboard = objective.getScoreboard();
         Collection<ScoreboardPlayerScore> playerRows = scoreboard.getAllPlayerScores(objective).stream().filter((score) -> score.getPlayerName() != null && !score.getPlayerName().startsWith("#")).collect(Collectors.toList());
 
@@ -77,13 +69,13 @@ public abstract class InGameHudMixin {
         for (Pair<ScoreboardPlayerScore, Text> pair : rowNamePair) {
             ++inc;
             int currentY = m - inc * 9;
-            fill(matrices, xOffset, currentY, this.scaledWidth, currentY + 9, backgroundColorLight);
-            tr.draw(matrices, pair.getSecond(), (float) x, (float) currentY, -1);
+            context.fill(xOffset, currentY, this.scaledWidth, currentY + 9, backgroundColorLight);
+            context.drawText(tr, pair.getSecond(), x, currentY, -1, false);
 
             if (inc == playerRows.size()) {
-                fill(matrices, xOffset, currentY - 9 - 1, this.scaledWidth, currentY - 1, backgroundColorDark);
-                fill(matrices, xOffset, currentY - 1, this.scaledWidth, currentY, backgroundColorLight);
-                tr.draw(matrices, text, (float) (x + textWidth / 2 - textWidth2 / 2), (float) (currentY - 9), -1);
+                context.fill(xOffset, currentY - 9 - 1, this.scaledWidth, currentY - 1, backgroundColorDark);
+                context.fill(xOffset, currentY - 1, this.scaledWidth, currentY, backgroundColorLight);
+                context.drawText(tr, text, (x + textWidth / 2 - textWidth2 / 2), currentY - 9, -1, false);
             }
         }
         ci.cancel();
