3fb36e7d6262131d58939a29068a6a10b7d64daa
BuildTools <w8888mc@gmail.com>
2023-12-22 04:15:52 +0000

    Hello again

--- src/main/java/me/pigalala/oinkscoreboard/OinkScoreboard.java
+++ src/main/java/me/pigalala/oinkscoreboard/OinkScoreboard.java
@@ -14,22 +14,8 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class OinkScoreboard implements ClientModInitializer {
 
-    private static final Identifier MESSAGE_CHANNEL = new Identifier("oinkscoreboard:settings");
-
     @Override
     public void onInitializeClient() {
         OinkConfig.load();
-
-        ClientPlayConnectionEvents.JOIN.register((clientPlayNetworkHandler, packetSender, client) -> {
-            if (client != MinecraftClient.getInstance()) return;
-            OinkScoreboard.sendRowsPacket();
-        });
-    }
-
-    public static void sendRowsPacket() {
-        PacketByteBuf packet = PacketByteBufs.create();
-        packet.writeByte(0);
-        packet.writeByte(OinkConfig.maxRows);
-        ClientPlayNetworking.send(MESSAGE_CHANNEL, packet);
     }
 }
